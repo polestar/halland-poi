222bba90b8f50c3df7a7f218aeacff73cfe11051
MarinovAnton <anton.marinov@polestar.com>
2023-06-28 13:24:07 +0200

    added favorite icons

--- automotive/src/main/java/com/example/varbergpoi/SubCategoryListScreen.kt
+++ automotive/src/main/java/com/example/varbergpoi/SubCategoryListScreen.kt
@@ -5,10 +5,12 @@ import androidx.car.app.CarToast
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
 import androidx.car.app.model.ActionStrip
+import androidx.car.app.model.CarIcon
 import androidx.car.app.model.ItemList
 import androidx.car.app.model.ListTemplate
 import androidx.car.app.model.Row
 import androidx.car.app.model.Template
+import androidx.core.graphics.drawable.IconCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import com.example.varbergpoi.dummydata.SubCategory
 
@@ -46,8 +48,10 @@ class SubCategoryListScreen(
         }
 
         val settings = Action.Builder()
-            .setTitle(
-                "Action title"
+            .setIcon(
+                CarIcon.Builder(
+                    IconCompat.createWithResource(carContext, R.drawable.baseline_favorite_border_24)
+                ).build()
             )
             .setOnClickListener {
                 CarToast.makeText(
